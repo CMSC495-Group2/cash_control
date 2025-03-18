82a004ca6806816f6ef35e1dc9202b308d23f6e1
Matthew Kahl <Matt@matthews-mbp.lan>
2025-03-18 15:19:48 -0400

    Added another line

--- TestCaseMattKahl.java
+++ TestCaseMattKahl.java
@@ -1,8 +1,9 @@
 public class TestCaseMattKahl {
 
     public static void main(String[] args) {
-        System.out.print("Hello, Classmtes!"); // Prints without a new line
+        System.out.print("Hello, Classmtes!"); 
         
 
+        System.out.print("At least im good for something!! - Matt");
     }
 }
