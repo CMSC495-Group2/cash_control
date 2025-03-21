e6ce5227b6a212b5d8bdce81383e57edf6d8940c
sllozier <sarah.lozier@gmail.com>
2025-03-21 10:48:00 -0400

    file structure skeleton

---
+++ frontend/src/App.js
@@ -0,0 +1,10 @@
+import "./index.css";
+import React from "react";
+import {Routes, Route, Navigate, Outlet} from "react-router-dom";
+
+
+function App (){
+    return(
+        <h1>Hello World!</h1>
+    )
+}
