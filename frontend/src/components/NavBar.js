c16dd7d03a369c20b9036c456e34462c501fcb59
sllozier <sarah.lozier@gmail.com>
2025-03-22 17:39:55 -0400

    github icon added

--- frontend/src/components/NavBar.js
+++ frontend/src/components/NavBar.js
@@ -1,5 +1,6 @@
 import React from "react";
 import logo from "../assets/images/cash_control_dark_logo.png";
+import github from "../assets/images/github-brands.svg"
 
 function NavBar(){
     return(
@@ -10,7 +11,7 @@ function NavBar(){
             <ul className="navbar-links">
                 <li><a href="#features">Features</a></li>
                 <li><a href="#start">Get Started</a></li>
-                <li><a href="https://github.com/CMSC495-Group2" target="_blank" rel="noreferrer">Github</a></li>
+                <li><a href="https://github.com/CMSC495-Group2" target="_blank" rel="noreferrer"><img src={github} alt="github logo" style={{ width: "24px", height: "24px" }}/></a></li>
             </ul>
         </nav>
     )
