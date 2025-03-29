765390812177a1b15ff5e884e68ef2cecfdadde7
sllozier <sarah.lozier@gmail.com>
2025-03-29 08:00:39 -0400

    remove duplicate maven files from root

--- TestCaseMattKahl.java
+++
@@ -1,9 +0,0 @@
-public class TestCaseMattKahl {
-
-    public static void main(String[] args) {
-        System.out.print("Hello, Classmtes!"); 
-        
-
-        System.out.print("At least im good for something!! - Matt");
-    }
-}
